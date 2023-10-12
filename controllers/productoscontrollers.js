e7058c10d11a9dcaeedf62ef0684373728229bfb
Gabriel Aron Arias <aron9377@gmail.com>
2023-10-12 15:33:23 -0300

    correciones en el controlador de productos

--- controllers/productoscontrollers.js
+++ controllers/productoscontrollers.js
@@ -31,7 +31,7 @@ const obtenerProductosPorId = async (req, res) => {
 
 const addProductos = async (req, res) => {
   try {
-    const producto = new CanchaModel(req.body);
+    const producto = new ProductosModel(req.body);
     await producto.save();
     res.status(201).json(producto);
   } catch (error) {
@@ -44,17 +44,17 @@ const addProductos = async (req, res) => {
 const updateProductos = async (req, res) => {
   try {
     const id = req.params.id;
-    const cancha = await CanchaModel.findById(id);
-    if (cancha) {
-      cancha.numero = req.body.numero;
-      cancha.capacidad = req.body.capacidad;
-      cancha.precio = req.body.precio;
-      cancha.img = req.body.img;
-      const canchaActualizada = await cancha.save();
-      res.status(200).json("Cancha actualizada");
-      res.status(canchaActualizada);
+    const producto = await ProductosModel.findById(id);
+    if (producto) {
+      producto.numero = req.body.numero;
+      producto.capacidad = req.body.capacidad;
+      producto.precio = req.body.precio;
+      producto.img = req.body.img;
+      const productoActualizada = await producto.save();
+      res.status(200).json("Producto actualizado");
+      res.status(productoActualizada);
     } else {
-      res.status(400).json("Cancha no encontrada ");
+      res.status(400).json("Producto no encontrado ");
     }
   } catch (error) {
     res.status(404).json("cancha no encontrada");
@@ -66,22 +66,22 @@ const updateProductos = async (req, res) => {
 const deleteProductos = async (req, res) => {
   try {
     const id = req.params.id;
-    const cancha = await CanchaModel.findById(id);
+    const cancha = await ProductosModel.findById(id);
     if (cancha) {
-      await CanchaModel.deleteOne({ _id: id });
-      res.status(200).json("Cancha eliminada");
+      await ProductosModel.deleteOne({ _id: id });
+      res.status(200).json("Producto eliminado");
     } else {
-      res.status(404).json("Cancha no encontrada");
+      res.status(404).json("Producto no encontrado");
     }
   } catch (error) {
-    res.status(400).json("Cancha no eliminada");
+    res.status(400).json("Producto no eliminado");
   }
 };
 
 module.exports = {
-    obtenerCanchas,
-    obtenerCanchasPorId,
-    addCancha,
-    updateCancha,
-    deleteCancha
-}
+  obtenerProductos,
+  obtenerProductosPorId,
+  addProductos,
+  updateProductos,
+  deleteProductos,
+};
