797f555f2bb3a8695a07d2999d4fe788e1d7b195
Gabriel Aron Arias <aron9377@gmail.com>
2023-10-18 16:31:03 -0300

    actualizacion de rutas productos

--- routes/Rutas-productos.js
+++ routes/Rutas-productos.js
@@ -4,19 +4,19 @@ const canchasController = require("../controllers/canchacontrollers");
 
 // Rutas del GET
 
-router.get("/productos", canchasController.obtenerCanchas);
-router.get("/productos/:id", canchasController.obtenerCanchasPorId);
+router.get("/productos", canchasController.obtenerProductos);
+router.get("/productos/:id", canchasController.obtenerProductosPorId);
 
-// Rutas de Post
+//Rutas de Post
 
-// router.post("/canchas", canchasController.addCancha);
+router.post("/canchas", canchasController.addProductos);
 
-// // Rutas de put
-// router.put("/canchas/:id", canchasController.updateCancha);
+// Rutas de put
+router.put("/canchas/:id", canchasController.updateProductos);
 // router.put("/canchas/reserva/:id", canchasController.reservarCancha);
 
-// // Rutas del delete
-// router.delete("/canchas/:id", canchasController.deleteCancha);
+// Rutas del delete
+router.delete("/canchas/:id", canchasController.deleteProductos);
 // router.delete("/canchas/reserva/:id", canchasController.eliminarReserva);
 
 
